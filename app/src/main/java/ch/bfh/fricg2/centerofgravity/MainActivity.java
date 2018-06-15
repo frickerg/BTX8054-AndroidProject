20d8099e97922f5ca1e05344d159d23b0a5d94e9
Guillaume Fricker <fricker.g@zoho.com>
2018-06-15 15:08:52 +0200

    Further improving the gyroscope and made a first debug test

--- app/src/main/java/ch/bfh/fricg2/centerofgravity/MainActivity.java
+++ app/src/main/java/ch/bfh/fricg2/centerofgravity/MainActivity.java
@@ -1,77 +1,64 @@
 package ch.bfh.fricg2.centerofgravity;
 
-import java.lang.Math;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.hardware.SensorManager;
+import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
 import android.content.Context;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    private static final float EPSILON = 0.000000001f;
-    private static final float NS2S = 1.0f / 1000000000.0f;
-    private final float[] deltaRotationVector = new float[4];
-    private float timestamp;
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
 
-    private SensorManager mSensorManager;
-    private Sensor mSensor;
+    public static float x;
+    public static float y;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // Set window fullscreen and remove title bar, and force landscape orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer,
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        // This timestep's delta rotation to be multiplied by the current rotation
-        // after computing it from the gyro sample data.
-        if (timestamp != 0) {
-            final float dT = (event.timestamp - timestamp) * NS2S;
-            // Axis of the rotation sample, not normalized yet.
-            float axisX = event.values[0];
-            float axisY = event.values[1];
-            float axisZ = event.values[2];
-
-            // Calculate the angular speed of the sample
-            float omegaMagnitude = (float) Math.sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
-
-            // Normalize the rotation vector if it's big enough to get the axis
-            // (that is, EPSILON should represent your maximum allowable margin of error)
-            if (omegaMagnitude > EPSILON) {
-                axisX /= omegaMagnitude;
-                axisY /= omegaMagnitude;
-                axisZ /= omegaMagnitude;
-            }
-
-            // Integrate around this axis with the angular speed by the timestep
-            // in order to get a delta rotation from this sample over the timestep
-            // We will convert this axis-angle representation of the delta rotation
-            // into a quaternion before turning it into the rotation matrix.
-            float thetaOverTwo = omegaMagnitude * dT / 2.0f;
-            float sinThetaOverTwo = (float) Math.sin(thetaOverTwo);
-            float cosThetaOverTwo = (float) Math.cos(thetaOverTwo);
-            deltaRotationVector[0] = sinThetaOverTwo * axisX;
-            deltaRotationVector[1] = sinThetaOverTwo * axisY;
-            deltaRotationVector[2] = sinThetaOverTwo * axisZ;
-            deltaRotationVector[3] = cosThetaOverTwo;
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            x = event.values[0];
+            System.out.println(x);
+            y = event.values[1];
+            ImageView paddleTop = findViewById(R.id.paddleTop);
+            paddleTop.setX(paddleTop.getX() + x);
         }
-        timestamp = event.timestamp;
-        float[] deltaRotationMatrix = new float[9];
-        SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
-        // User code should concatenate the delta rotation we computed with the current rotation
-        // in order to get the updated rotation.
-        // rotationCurrent = rotationCurrent * deltaRotationMatrix;
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy){
-        if (sensor == this.mSensor) {
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        if (sensor == this.accelerometer) {
             switch (accuracy) {
                 case 0:
                     System.out.println("Unreliable");
